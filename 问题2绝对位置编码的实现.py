650d8e1bd0ff0c6bea80334602dbdb89a69934fd
691292605 <145271671+691292605@users.noreply.github.com>
2024-09-19 11:19:46 +0800

    Update 问题2绝对位置编码的实现.py

--- 问题2绝对位置编码的实现.py
+++ 问题2绝对位置编码的实现.py
@@ -1,7 +1,5 @@
 import torch
 import torch.nn as nn
-import math
-
 
 """
 实现绝对编码较为简单，理解它是作为位置编码直接与词向量矩阵相加即可在词向量上添加位置信息即可
@@ -9,21 +7,20 @@ import math
 """
 
 class Encoding(nn.Module):
-    def __init__(self, max_len, dim):
+    def __init__(self, seq_len, dim):
         super(Encoding, self).__init__()
         if dim % 2 != 0:    #在代码编写完之后测试发现，该绝对编码过程无法处理维度为奇数的情况，故这里增加了对奇数的处理情况
             dim += 1
             self.is_odd = True
-        pe = torch.zeros(max_len, dim)
-        position = torch.arange(0, max_len).unsqueeze(1)
-        func = torch.arange(0, dim, 2)
-        func = torch.exp(func * (-math.log(10000.0) / dim))
+        pe = torch.zeros(seq_len, dim)
+        position = torch.arange(0, seq_len).unsqueeze(1)
+        func = 1 / (10000 ** (torch.arange(0, dim, 2) // dim)).float()
         pe[:, 0::2] = torch.sin(position * func)
         pe[:, 1::2] = torch.cos(position * func)
         #具体公式的推导和运行的过程在note里
         """
         直接在这里进行位置编码的计算是因为每个位置的位置编码都是固定的，与参数无关，只与位置有关
-        也就是说在init中直接计算可以避免在下面的函数中进行重复计算
+        即在init中直接计算可以避免在下面的函数中进行重复计算
         """
         if self.is_odd:
             pe = pe[:, :-1]
@@ -33,12 +30,10 @@ class Encoding(nn.Module):
         seq_len = data.size(1)
         return data + self.pe[:, :seq_len]
 
-
-#举例
-Len = 10
-d = 5
-model = Encoding(Len, d)
-x = torch.randn(2, 5, d)
+#示例
+x = torch.randn(3, 5)
+seq, d = x.size()
+model = Encoding(seq, d)
 output = model(x)
 print("x:")
 print(x)
